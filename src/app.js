89b28e741f3bc7b614826a526d6ed8aad6d1112f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-08 10:58:39 -0500

    feat: add a heavy response

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.get('/stringlargo', (req, res) => {
     let string = 'Este string es muy largo para la request!!!'
+
+    for (let index = 0; index < 10e4; index++) {
+        string += '<br>Estamos haciendo aun mas larga la respuesta!</br>'
+    }
     res.send(string)
 })
 
