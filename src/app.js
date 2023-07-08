2892207ac033e76feb4dfbeb0a20ef6102e124b8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-08 11:09:12 -0500

    feat: add compression

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import compression from 'express-compression'
+// import compression from 'express-compression'
+import compression from 'compression'
 
 const app = express()
-app.use(compression())
+app.use(compression({
+    level: 1
+}))
 
 app.get('/stringlargo', (req, res) => {
     let string = 'Este string es muy largo para la request!!!'
@@ -11,6 +14,11 @@ app.get('/stringlargo', (req, res) => {
         string += '<br>Estamos haciendo aun mas larga la respuesta!</br>'
     }
     res.send(string)
+})
+
+app.get('/otrostringlargo', (req, res) => {
+    const payload = 'Hello World from Coder!!'
+    res.send(payload.repeat(100000))
 })
 
 app.listen(8080, () => console.log('Server Up!'))
